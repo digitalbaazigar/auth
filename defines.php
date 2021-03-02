ce9fa59ff9296c56fdaa712d8583fb6088dc4d24
digitalbaazigar <79902062+digitalbaazigar@users.noreply.github.com>
2021-03-02 14:35:22 +0530

    Create defines.php

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '473049847041046' );
+	define( 'INSTAGRAM_APP_SECRET', '9ec2131e4a656a822f6b958080e15b44' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://github.com/digitalbaazigar/auth/' );
